22d1629445606141110ce5ec7756d0090d9baef8
Preethi Rajavel <88610074+pretipreti@users.noreply.github.com>
2022-10-10 10:59:32 +0530

    Add files via upload

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,14 @@
+public class CheckPositiveOrNegative{
+    public static void main(String[]args){
+        int num=18;
+        if(num>0){
+            System.out.println("The number is positive");
+        }
+        else if(num<0){
+            System.out.println("The number is negative");
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+    }
+}
